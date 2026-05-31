0b955d065719b4e91e54061aaafd352929515743
krystallfabrik <112928276+krystallfabrik@users.noreply.github.com>
2026-05-31 17:22:38 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-2" 
+}
+
+variable "s3_bucket" {
+       description = "The name of the bucket"
+       default    = "kfabrik-645975910827-300526-bucket-hv4u8jx11xdjjm"
+}
